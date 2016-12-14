310301daa55bce0c350b3e6ae3ac7d6a8a7f12c0
Jon Ramer <jramer87@gmail.com>
2016-12-14 10:39:11 -0600

    finished routes

--- migrations/20161212141513_books.js
+++ migrations/20161212141513_books.js
@@ -1,15 +1,26 @@
-exports.up = function (knex, Promise) {
+exports.up = function (knex) {
   return knex.schema.createTable('books', (table) => {
-    table.increments('id').primary();
-    table.string('title').notNullable().defaultTo('');
-    table.string('author').notNullable().defaultTo('');
-    table.string('genre').notNullable().defaultTo('');
-    table.text('description').notNullable().defaultTo('');
-    table.text('cover_url').notNullable().defaultTo('');
+    table.increments('id')
+            .primary();
+    table.string('title')
+            .notNullable()
+            .defaultTo('');
+    table.string('author')
+            .notNullable()
+            .defaultTo('');
+    table.string('genre')
+            .notNullable()
+            .defaultTo('');
+    table.text('description')
+            .notNullable()
+            .defaultTo('');
+    table.text('cover_url')
+            .notNullable()
+            .defaultTo('');
     table.timestamps(true, true);
   });
 };
 
-exports.down = function (knex, Promise) {
+exports.down = function (knex) {
   return knex.schema.dropTable('books');
 };
--- seeds/books.js
+++ seeds/books.js
@@ -1,10 +1,13 @@
 const data = require('../config/books.js');
 
 exports.seed = function seed(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('books').del()
-  .then(() => Promise.all([
-      // Inserts seed entries
-    knex('books').insert(data.books),
-  ]));
+    // Deletes ALL existing entries
+    return knex('books')
+        .del()
+        .then(() => Promise.all([
+            // Inserts seed entries
+            knex('books')
+            .insert(data.books),
+            knex.raw('SELECT setval(\'books_id_seq\', (SELECT MAX(id) FROM books))'),
+        ]));
 };
